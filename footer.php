57e9d03787a5c7206be9ef280d9fc7c019035a61
児玉恵吾 <k.kei5.0205@gmail.com>
2022-08-02 20:23:27 +0900

    20220802

---
+++ footer.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * @package    WordPress
+ * @subpackage Maitake
+ * @since      1.0.0
+ */
+?>
+	</main>
+	<footer>フッター</footer>
+	<?php wp_footer(); ?>
+</body>
+</html>
