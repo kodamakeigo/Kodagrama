26aaf337c70dc0e0cb46cdc1cd860373f97359e3
児玉恵吾 <k.kei5.0205@gmail.com>
2022-11-22 20:17:31 +0900

    20221113修正

--- home.php
+++ home.php
@@ -28,7 +28,7 @@ get_header(); ?>
 </section>
 
 <section>
-    <h2>About</h2>
+    <h2 class="title-home">About</h2>
     <div class="home-about-list-wrap">
         <ul id="js-home-about-list" class="home-about-list">
             <li>
@@ -51,33 +51,32 @@ get_header(); ?>
     </div>
 </section>
 <section>
-    <h2>Works</h2>
+    <h2 class="title-home">Works</h2>
     <div class="home-about-list-wrap">
         <ul class="home-works-list">
-            <div class="familycard_wrap">
-                <li class="familycard">家族名刺</li>
-                <img src="<?php echo esc_url(get_template_directory_uri()); ?>/images/family_flont.png" class="familycard_flont" alt="家族名刺画像表" width="" height="" loading="lazy">
-                <img src="<?php echo esc_url(get_template_directory_uri()); ?>/images/family_back.png" class="familycard_back" alt="家族名刺画像裏" width="" height="" loading="lazy">
+            <li class="card_wrap">
+                <h3 class="familycard">家族名刺</h3>
+                <img src="<?php echo esc_url(get_template_directory_uri()); ?>/images/family_flont.png" class="crad_img" alt="家族名刺画像表" width="" height="" loading="lazy">
+                <img src="<?php echo esc_url(get_template_directory_uri()); ?>/images/family_back.png" class="crad_img" alt="家族名刺画像裏" width="" height="" loading="lazy">
                 <p>イラストデザインを始めて行った作品です。<br>アットホームな色使い、シンプルで個人の特徴を捉えたイラストを意識しました。</p>
-            </div>
-            <div class="maicard_wrap">
-                <li class="maicard">妻の名刺</li>
-                <img src="<?php echo esc_url(get_template_directory_uri()); ?>/images/mai_flont.png" class="maicard_flont" alt="妻の名刺画像" width="" height="" loading="lazy">
+            </li>
+            <li class="card_wrap">
+                <h3 class="maicard">妻の名刺</h3>
+                <img src="<?php echo esc_url(get_template_directory_uri()); ?>/images/mai_flont.png" class="crad_img" alt="妻の名刺画像" width="" height="" loading="lazy">
                 <p>妻が就職した不動産会社の名刺をデザインしました。<br>モノクロのシンプルな配色を意識し、顔写真有など細かな要望に答える形で作成しました。</p>
-            </div>
-            <div class="kscard_wrap">
-                <li class="kscard">オカンの名刺</li>
-                <img src="<?php echo esc_url(get_template_directory_uri()); ?>/images/ks_flont.png" class="kscard_flont" alt="オカンの名刺画像表" width="" height="" loading="lazy">
-                <img src="<?php echo esc_url(get_template_directory_uri()); ?>/images/ks_back.png" class="familycard_back" alt="オカンの名刺画像裏" width="" height="" loading="lazy">
-                <p>オカンの不動産管理会社名刺をデザインしました。<br>北海道をモチーフにしてとのことでしたので、かわいい動物と
-                    北海道拠点の富良野から見える十勝岳連峰の写真をイラスト風に加工しました。</p>
-            </div>
+            </li>
+            <li class="card_wrap">
+                <h3 class="kscard">オカンの名刺</h3>
+                <img src="<?php echo esc_url(get_template_directory_uri()); ?>/images/ks_flont.png" class="crad_img" alt="オカンの名刺画像表" width="" height="" loading="lazy">
+                <img src="<?php echo esc_url(get_template_directory_uri()); ?>/images/ks_back.png" class="crad_img" alt="オカンの名刺画像裏" width="" height="" loading="lazy">
+                <p>オカンの不動産管理会社名刺をデザインしました。<br>北海道をモチーフにしてとのことでしたので、かわいい動物と北海道拠点の富良野から見える十勝岳連峰の写真をイラスト風に加工しました。</p>
+            </li>
         </ul>
     </div>
 </section>
 
 <section>
-    <h2>Contact</h2>
+    <h2 class="title-home">Contact</h2>
     <?php echo do_shortcode('[contact-form-7 id="17" title="お問い合わせフォーム"]'); ?>
 </section>
 <?php get_footer();
