5b41ff1c00b9e61aa10d76f89e510dbd2a3cdfa1
児玉恵吾 <k.kei5.0205@gmail.com>
2023-01-10 02:11:02 +0900

    年末年始課題　フッター

--- footer.php
+++ footer.php
@@ -12,6 +12,9 @@
 <footer class="footer">
     <a href="#" class="page-top-btn"><span class="screen-reader-text">ページトップへ</span></a>
     <!-- <img src="<?php echo esc_url(get_template_directory_uri()); ?>/images/footer_back.png" class="footer_back" alt="背景" width="1280" height="300" loading="lazy"> -->
+    <img src="<?php echo esc_url(get_template_directory_uri()); ?>/images/pc-footer-crabs.png" class="pc-footer-crabs" alt="footer-crabs" width="1380" height="300" loading="eager">
+    <img src="<?php echo esc_url(get_template_directory_uri()); ?>/images/sp-footer-crab.png" class="sp-footer-crab" alt="footer-crab" width="1380" height="300" loading="eager">
+    <span class="copyright">&copy;kodagrama</span>
 </footer>
 <?php wp_footer(); ?>
 </body>
